f5d7f51efa0b3b8e1d9e51f60847cd4ed1e5f09b
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 12:14:45 +0630

    -

--- DotNet8WebApi.ExceptionHandlingMiddleware/Resources/MessageResource.cs
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Resources/MessageResource.cs
@@ -1,14 +1,13 @@
-﻿namespace DotNet8WebApi.ExceptionHandlingMiddleware.Resources
+﻿namespace DotNet8WebApi.ExceptionHandlingMiddleware.Resources;
+
+public class MessageResource
 {
-    public class MessageResource
-    {
-        public static string Success { get; } = "Success.";
-        public static string SaveSuccess { get; } = "Saving Successful.";
-        public static string SaveFail { get; } = "Saving Fail.";
-        public static string UpdateSuccess { get; } = "Updating Successful.";
-        public static string UpdateFail { get; } = "Updating Fail.";
-        public static string DeleteSuccess { get; } = "Deleting Successful.";
-        public static string DeleteFail { get; } = "Deleting Fail.";
-        public static string NotFound { get; } = "No Data Found.";
-    }
+    public static string Success { get; } = "Success.";
+    public static string SaveSuccess { get; } = "Saving Successful.";
+    public static string SaveFail { get; } = "Saving Fail.";
+    public static string UpdateSuccess { get; } = "Updating Successful.";
+    public static string UpdateFail { get; } = "Updating Fail.";
+    public static string DeleteSuccess { get; } = "Deleting Successful.";
+    public static string DeleteFail { get; } = "Deleting Fail.";
+    public static string NotFound { get; } = "No Data Found.";
 }
