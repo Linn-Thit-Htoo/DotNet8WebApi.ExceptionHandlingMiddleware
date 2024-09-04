a038562bc8cbf3fc54655567ebe8e93f7349fef0
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 10:47:14 +0630

    Add project files.

---
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Controllers/SampleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8WebApi.ExceptionHandlingMiddleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult SampleEndpoint()
+        {
+            return Ok("Hello!");
+        }
+    }
+}
---
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Middlewares/OldExceptionHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace DotNet8WebApi.ExceptionHandlingMiddleware.Middlewares
+{
+    public class OldExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<OldExceptionHandlingMiddleware> _logger;
+
+        public OldExceptionHandlingMiddleware(RequestDelegate next, ILogger<OldExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                var problemDetails = new
+                {
+                    Status = 500,
+                    Title = "Internal Server Error"
+                };
+
+                context.Response.StatusCode = problemDetails.Status;
+                await context.Response.WriteAsJsonAsync(problemDetails);
+            }
+        }
+    }
+}
