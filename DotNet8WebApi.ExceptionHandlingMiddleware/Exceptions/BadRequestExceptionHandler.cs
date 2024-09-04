277e1d8626340eb378827152cb397dd9030ce88c
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 12:13:23 +0630

    -

--- DotNet8WebApi.ExceptionHandlingMiddleware/Exceptions/BadRequestExceptionHandler.cs
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Exceptions/BadRequestExceptionHandler.cs
@@ -1,31 +1,34 @@
 ﻿using DotNet8WebApi.ExceptionHandlingMiddleware.Models;
 using Microsoft.AspNetCore.Diagnostics;
 
-namespace DotNet8WebApi.ExceptionHandlingMiddleware.Exceptions
+namespace DotNet8WebApi.ExceptionHandlingMiddleware.Exceptions;
+
+public class BadRequestExceptionHandler : IExceptionHandler
 {
-    public class BadRequestExceptionHandler : IExceptionHandler
+    private readonly ILogger<BadRequestExceptionHandler> _logger;
+
+    public BadRequestExceptionHandler(ILogger<BadRequestExceptionHandler> logger)
     {
-        private readonly ILogger<BadRequestExceptionHandler> _logger;
+        _logger = logger;
+    }
 
-        public BadRequestExceptionHandler(ILogger<BadRequestExceptionHandler> logger)
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
+    {
+        if (exception is not BadHttpRequestException)
         {
-            _logger = logger;
+            return false;
         }
 
-        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
-        {
-            if (exception is not BadHttpRequestException)
-            {
-                return false;
-            }
+        _logger.LogError(exception.ToString());
+        var result = Result<string>.Fail(exception);
 
-            _logger.LogError(exception.ToString());
-            var result = Result<string>.Fail(exception);
+        httpContext.Response.StatusCode = Convert.ToInt32(result.StatusCode);
+        await httpContext.Response.WriteAsJsonAsync(result, cancellationToken: cancellationToken);
 
-            httpContext.Response.StatusCode = Convert.ToInt32(result.StatusCode);
-            await httpContext.Response.WriteAsJsonAsync(result, cancellationToken: cancellationToken);
-
-            return true;
-        }
+        return true;
     }
 }
