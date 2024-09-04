f092c97d922ddb359a2030246b4f23ff63f1e922
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 12:13:40 +0630

    -

--- DotNet8WebApi.ExceptionHandlingMiddleware/Exceptions/GlobalExceptionHandler.cs
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Exceptions/GlobalExceptionHandler.cs
@@ -12,14 +12,17 @@ public class GlobalExceptionHandler : IExceptionHandler
         _logger = logger;
     }
 
-    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
     {
         _logger.LogError(exception.ToString());
         var result = Result<string>.Fail(exception.ToString());
 
         httpContext.Response.StatusCode = Convert.ToInt32(result.StatusCode);
-        await httpContext.Response
-            .WriteAsJsonAsync(result, cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync(result, cancellationToken);
 
         return true;
     }
