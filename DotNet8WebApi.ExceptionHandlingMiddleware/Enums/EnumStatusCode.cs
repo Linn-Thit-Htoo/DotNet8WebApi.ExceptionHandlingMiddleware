14d7fd154b91f347b599982868a66e83569fde9f
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 12:13:13 +0630

    -

--- DotNet8WebApi.ExceptionHandlingMiddleware/Enums/EnumStatusCode.cs
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Enums/EnumStatusCode.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNet8WebApi.ExceptionHandlingMiddleware.Enums
+﻿namespace DotNet8WebApi.ExceptionHandlingMiddleware.Enums;
+
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        None,
-        OK = 200,
-        Created = 201,
-        Accepted = 202,
-        BadRequest = 400,
-        NotFound = 404,
-        Conflict = 409,
-        Locked = 423,
-        InternalServerError = 500
-    }
+    None,
+    OK = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    NotFound = 404,
+    Conflict = 409,
+    Locked = 423,
+    InternalServerError = 500
 }
