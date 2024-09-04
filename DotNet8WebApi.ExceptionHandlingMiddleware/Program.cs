e14320757e9f8d39343e238fd6aa9c1fd06a9c2d
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 12:15:02 +0630

    -

--- DotNet8WebApi.ExceptionHandlingMiddleware/Program.cs
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Program.cs
@@ -1,14 +1,8 @@
-
-
 using DotNet8WebApi.ExceptionHandlingMiddleware.Exceptions;
-using DotNet8WebApi.ExceptionHandlingMiddleware.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,7 +12,6 @@ builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
