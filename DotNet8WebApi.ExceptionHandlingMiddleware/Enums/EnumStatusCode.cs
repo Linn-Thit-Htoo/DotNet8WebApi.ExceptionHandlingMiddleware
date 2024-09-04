5dbb513d784f285b0f5aa6599c54a6ce60f83f9d
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 10:56:54 +0630

    [ Added ] Result Pattern

---
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Enums/EnumStatusCode.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNet8WebApi.ExceptionHandlingMiddleware.Enums
+{
+    public enum EnumStatusCode
+    {
+        None,
+        OK = 200,
+        Created = 201,
+        Accepted = 202,
+        BadRequest = 400,
+        NotFound = 404,
+        Conflict = 409,
+        Locked = 423,
+        InternalServerError = 500
+    }
+}
---
+++ DotNet8WebApi.ExceptionHandlingMiddleware/Models/Result.cs
@@ -0,0 +1,29 @@
+﻿using DotNet8WebApi.ExceptionHandlingMiddleware.Enums;
+
+namespace DotNet8WebApi.ExceptionHandlingMiddleware.Models
+{
+    public class Result<T>
+    {
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public EnumStatusCode StatusCode { get; set; }
+
+        public static Result<T> Success(string message = "Success.")
+        {
+            return new Result<T> { Message = message, StatusCode = EnumStatusCode.OK, IsSuccess = true };
+        }
+        public static Result<T> Success(T data, string message = "Success.")
+        {
+            return new Result<T> { Message = message, Data = data, StatusCode = EnumStatusCode.OK, IsSuccess = true };
+        }
+        public static Result<T> Fail(string message = "Fail.")
+        {
+            return new Result<T> { Message = message, StatusCode = EnumStatusCode.BadRequest, IsSuccess = false };
+        }
+        public static Result<T> Fail(Exception ex)
+        {
+            return new Result<T> { Message = ex.ToString(), StatusCode = EnumStatusCode.InternalServerError, IsSuccess = false };
+        }
+    }
+}
